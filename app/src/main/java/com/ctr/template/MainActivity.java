e4a09016dfc273e2a244ec04c200a739b263dad8
ruddycondori <rucocool@gmail.com>
2020-04-02 17:44:33 -0400

    Configuracion Inicial de Template

--- app/src/main/java/com/ctr/template/MainActivity.java
+++ app/src/main/java/com/ctr/template/MainActivity.java
@@ -1,26 +1,32 @@
 package com.ctr.template;
 
-import android.content.Context;
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
-//import com.github.moduth.blockcanary.BlockCanaryContext;
-
-//import dodola.blockindigo.BlockIndigo;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.txtPrueba)
+    TextView title;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //BlockIndigo.install(this, new BlockCanaryContext());
         setContentView(R.layout.activity_main);
 
-        Context context = getApplicationContext();
-        Intent menu = new Intent(context,MenuActivity.class);
-        menu.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        context.startActivity(menu);
-
+        ButterKnife.bind(this);
+        title.setText("agregando");
     }
+
+    @OnClick(R.id.txtPrueba)
+    public void mostrarMensaje() {
+        Toast.makeText(MainActivity.this, "Mostrando", Toast.LENGTH_SHORT).show();
+    }
+
 }
